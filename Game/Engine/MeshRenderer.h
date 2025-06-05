bc29bf9d030f9ba23cc576ae8656af609eae0fb4
Lim Sooyoung <djwjod@naver.com>
2025-06-05 17:40:33 +0900

    Instancing 완료

--- Game/Engine/MeshRenderer.h
+++ Game/Engine/MeshRenderer.h
@@ -4,6 +4,16 @@
 class Mesh;
 class Material;
 
+union InstanceID
+{
+	struct
+	{
+		uint32 meshID;
+		uint32 materialID;
+	};
+	uint64 id;
+};
+
 class MeshRenderer : public Component
 {
 public:
@@ -16,6 +26,9 @@ public:
 	void SetMaterial(shared_ptr<Material> material) { _material = material; }
 
 	void Render();
+	void Render(shared_ptr<class InstancingBuffer>& buffer);
+
+	uint64 GetInstanceID();
 
 private:
 	shared_ptr<Mesh> _mesh;
