745fa2e9d794adb3a1077909e18b2c93c78ea9b1
Lim Sooyoung <djwjod@naver.com>
2025-07-07 14:32:03 +0900

    Picking 완료

--- Game/Engine/Input.cpp
+++ Game/Engine/Input.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "Input.h"
+#include "Engine.h"
 
 void Input::Init(HWND hwnd)
 {
@@ -46,4 +47,8 @@ void Input::Update()
 				state = KEY_STATE::NONE;
 		}
 	}
+
+	// ¸¶¿ì½º ÁÂÇ¥
+	::GetCursorPos(&_mousePos);
+	::ScreenToClient(GEngine->GetWindow().hwnd, &_mousePos);
 }
